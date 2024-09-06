9a8fa59ebecc8060f0a1e6bd37d2892bfac44e75
Emad Askareimotlagh <emad.askari@gmail.com>
2024-09-06 19:38:01 +0200

    Version 1.0.6

---
+++ src/types/react-multy-date-range.d.ts
@@ -0,0 +1,37 @@
+declare module 'react-multi-date-range' {
+    import * as React from 'react';
+    import { CSSProperties } from 'react';
+
+    type BeginDayOfWeek = 'sunday' | 'monday';
+
+    interface InputData {
+        startDate: Date;
+        endDate: Date;
+        key: string;
+        type: string;
+        color: string;
+        cellTextColor?: string;
+    }
+
+    interface StaticDatePickerProps {
+
+        loading: boolean;
+        theme?: string;
+        GuildComponent?: JSX.Element;
+        handleRangeClick: (item: InputData) => void;
+        onShownDateChange: (date: Date) => void;
+        activeDate: Date;
+        data: InputData[];
+        beginDayOfWeek: BeginDayOfWeek;
+        backgroundColor: string;
+        textColor: string;
+        borderColor?: string;
+        containerStyle?: CSSProperties;
+        width?: string;
+        guidHeaderColor?: string;
+    }
+
+    const ReactMultiDateRange: React.FC<StaticDatePickerProps>;
+
+    export default ReactMultiDateRange;
+}
